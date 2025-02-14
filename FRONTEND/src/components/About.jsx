768dbe7246706e5886d41ca10b9d46ebb829d222
Anish Mahajan <anishkm16@gmail.com>
2025-02-14 17:53:36 +0530

    first commit

---
+++ FRONTEND/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    
+    <section className='about container'>
+      <h4>EVENTS</h4>
+      <h2>ABOUT</h2>
+      <p>At LuxePlanner, we turn your dreams into reality with seamless event planning and execution. From intimate gatherings to grand celebrations, our expert team ensures every detail is perfect. We specialize in weddings, corporate events, parties, and more, crafting unforgettable experiences tailored to your vision. With creativity, precision, and passion, we handle everything—from venue selection to décor, catering, and entertainment. Our goal is to make your event stress-free and extraordinary. Let us bring your ideas to life and create moments that last a lifetime. Your event, our expertise! </p>
+    </section>
+  )
+}
+
+export default About
